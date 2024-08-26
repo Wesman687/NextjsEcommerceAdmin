4e12a135338ff9e318f85e65a9a7fd42c5b6f1da
Paul <wesman687@gmail.com>
2024-08-26 14:26:17 -0400

    added paid

--- app/orders/page.js
+++ app/orders/page.js
@@ -5,7 +5,6 @@ import axios from 'axios'
 
 export default function OrderPage() {
     const [orders, setOrders] = useState([])
-    console.log(orders)
     useEffect(()=>{
             axios.get('/api/orders').then(res =>{
                 setOrders(res.data)
@@ -16,14 +15,20 @@ export default function OrderPage() {
     <Layout>
       <h1>Orders</h1>
       <table className='basic'>
-        <th className='text-xl'>Date</th>
-        <th className='text-xl'>Recipient</th>
-        <th className='text-xl'>Products</th>
+        <thead>
+          <tr className='underline text-xl'>
+        <th className=''>Date</th>
+        <th>Paid</th>
+        <th className=''>Recipient</th>
+        <th className=''>Products</th>
+        </tr>
+        </thead>
       <tbody>
         {orders?.length > 0 && orders?.map((order)=>(
             <tr key={order._id}>
                <td>{(new Date(order.createdAt)).toLocaleString()}<br /> Total: ${order.total}</td>
-               <td className=' '>{order.name}<br />
+               <td className={order.paid ? 'text-green-600' : 'text-red-600'}>{order.paid ? "YES" : "No"}</td>
+               <td className='flex items-center '>{order.name}<br />
                {order.email}<br />
                {order.city} {order.state}<br />
                {order.address}<br />
@@ -31,7 +36,7 @@ export default function OrderPage() {
                </td>
                {order.line_items.map((product)=> (
                     <td className='text-right'>
-                        {product?.price_data?.product_data?.name}<br />     Quantity: {product?.quantity}      Price: ${(product?.price_data?.unit_amount / 1000).toFixed(2)}
+                        {product?.price_data?.product_data?.name}<br />     Quantity: {product?.quantity}      Price: ${(product?.price_data?.unit_amount / 100).toFixed(2)}
                     </td>
                ))}
 
